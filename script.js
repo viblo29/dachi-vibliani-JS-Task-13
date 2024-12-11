1396060b7e265cc282f2b1b0d8f0900f2eab1753
viblo29 <dachivibliani29@gmail.com>
2024-12-12 03:11:02 +0400

    1st

---
+++ script.js
@@ -0,0 +1,37 @@
+function fetchData() {
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then(Response => Response.json())
+.then(data => {
+        data.forEach(post => {console.log(post.body)})})
+    .catch(error => console.error((error)));
+}
+
+function createPost() {
+fetch("https://jsonplaceholder.typicode.com/posts",{
+    method:'POST',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+        title:`new post`,
+        body:`Lorem ipsum dorem`,
+        userId:1
+    })
+})
+ .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+}
+createPost();
+
+function deletePost() {
+    fetch("https://jsonplaceholder.typicode.com/posts/2", {
+    method:'DELETE',
+    headers: {
+        'Content-Type':'application/json'
+    }
+})
+    .then(response => console.log(`Post Deleted`))
+    .catch(error => console.log(error));
+}
+deletePost();
